176ada7a916cd4de3884834d284aa68611db4df3
KungPaoMeow <johnli8642@gmail.com>
2024-12-24 13:00:23 -0500

    more styles and random images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     console.log("Deleting old campgrounds...");
     for (let i = 0; i < 50; i++) {
         const randCity = sample(cities);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${randCity.city}, ${randCity.state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sapiente possimus. Quam dignissimos eum quod maxime suscipit dolore! Perspiciatis esse rem eligendi, provident libero similique commodi sequi nam totam amet!',
+            price: price
         });
         await camp.save();
     }
