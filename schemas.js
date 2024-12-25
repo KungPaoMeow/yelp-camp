90ad7b8c3059fa520f6693c2d7a3d4601363675b
KungPaoMeow <johnli8642@gmail.com>
2024-12-24 20:55:08 -0500

    express error handling and server-side data validation middleware (using joi)

---
+++ schemas.js
@@ -0,0 +1,12 @@
+/* Request body validation schemas */
+const Joi = require('joi');
+
+module.exports.campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
